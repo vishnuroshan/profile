edd8feccd66562766ccb060056e4633e9cecc754
vishnu <vishnu@sedintechnologies.com>
2024-05-16 01:04:02 +0530

    content changes

---
+++ src/lib/contact.svelte
@@ -0,0 +1,34 @@
+<div class="contact-info-container">
+    <div class="contact-item">
+      <a href="https://www.linkedin.com/in/vishnu-roshan" target="_blank">LinkedIn</a>
+    </div>
+    <div class="contact-item">
+      <a href="mailto:vishnuroshan4@gmail.com" target="_top">Email</a>
+    </div>
+    <div class="contact-item">
+      <a href="https://www.github.com/vishnuroshan" target="_blank">Github</a>
+    </div>
+  </div>
+
+  <style>
+    .contact-info-container {
+    margin: auto;
+    width: 50%
+  }
+
+  .contact-item {
+    border-radius: 5px;
+    margin: 1rem;
+    display: flex;
+    justify-content: center;
+    border-width: 1px;
+    padding: 1.5rem;
+    font-size: 16px;
+    border-style: solid;
+  }
+  .contact-item:hover {
+    border-style: double;
+    border-width: 0.2em;
+    margin: 0.5rem;
+  }
+  </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container col-xl-10 col-xxl-8 px-4 py-5">
+<div class="row align-items-center g-lg-5 py-5">
+      <div class="col-lg-7 text-center text-lg-start">
+        <h1 class="display-4 fw-bold lh-1 mb-3">👋<br>I am Vishnu Roshan</h1>
+        <p class="col-lg-10 fs-4">A software engineer with more than 6 years of experience in web technologies.</p>
+      </div>
+      <div class="col-md-10 mx-auto col-lg-5">
+        <form class="p-4 p-md-5 border rounded-3 bg-light">
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3 h-25">
+            <textarea rows="3" class="form-control h-25" id="messageInput" placeholder="Your message" />
+            <label for="messageInput">Message</label>
+          </div>
+          
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Lets connect</button>
+          <hr class="my-4">
+          <small class="text-muted">I usually take 2 to 3 days to respond, milage may vary!</small>
+        </form>
+      </div>
+    </div>
+</div>
+<div class="divider"></div>
+
+
+<style>
+  .divider {
+  height: 3rem;
+  background-color: rgba(0, 0, 0, .1);
+  border: solid rgba(0, 0, 0, .15);
+  border-width: 1px 0;
+  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+}
+</style>
+
