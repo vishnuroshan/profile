19de54afca32f1a9c63703025594a959aff10a24
vishnu <vishnu@sedintechnologies.com>
2024-05-16 01:37:29 +0530

    made the content shorter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <div class="container col-xl-10 col-xxl-8 px-4 py-5">
 <div class="row align-items-center g-lg-5 py-5">
       <div class="col-lg-7 text-center text-lg-start">
-        <h1 class="display-4 fw-bold lh-1 mb-3">👋<br>I am Vishnu Roshan</h1>
-        <p class="col-lg-10 fs-4">A software engineer with more than 6 years of experience in web technologies.</p>
+        <h1 class="display-4 fw-bold lh-1 mb-3">👋<br>I am Vishnu Roshan,</h1>
+        <p class="col-lg-10 fs-4">I make fast, profitable & pleasant web experiences.</p>
       </div>
       <div class="col-md-10 mx-auto col-lg-5">
         <form class="p-4 p-md-5 border rounded-3 bg-light">
@@ -22,16 +22,37 @@
       </div>
     </div>
 </div>
-<div class="divider"></div>
+<div class="divider">
+ <span class="bar_content">
+     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+   </span>
+   
+</div>
 
 
 <style>
   .divider {
-  height: 3rem;
+  color: black;
+  height: fit-content;
+  min-height: 2rem;
   background-color: rgba(0, 0, 0, .1);
   border: solid rgba(0, 0, 0, .15);
   border-width: 1px 0;
   box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+}
+
+.bar_content {
+  justify-content: center;
+  text-align: center;
+  display: block; /* Important to give the content a width */
+  width: 100%;
+  transform: translateX(100%); /* Animation start out of the screen */
+  /* Add the animation */
+  animation: move 30s linear infinite /* infinite make reapeat the animation indefinitely */;
+}
+/* Create the animation */
+@keyframes move {
+  to { transform: translateX(-100%); }
 }
 </style>
 
